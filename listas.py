00dc2ba88a7baa44c8ee7f3ac5d4f7ef0bac0bef
Pan_Batido <escarleth.canon@gmail.com>
2025-06-10 16:33:49 -0400

    10-06

---
+++ listas.py
@@ -0,0 +1,45 @@
+#lista.append() genera un dato al final de la lista
+#lista.insert(3.100) genera un dato en un lugar asignado de la lista
+
+# frutas=["manzana", "mango", "membrillo"]
+
+# for fruta in frutas:
+#     print(fruta)
+
+# nombres=["hideki"]
+# apellidos=["anno"]
+# while True:
+#     print(''' 
+#         1.- Insertar nombre y apellidos
+#         2.- Mostrar nombre y apellidos
+#         3.- Buscar nombre
+#         4.- Salir
+#         ''')
+#     op=int(input("seleccione una opción: "))
+#     match op:
+#         case 1:
+#             nombre=input("ingrese un nombre: ")
+#             nombres.append(nombre)
+#             apellido=input("ingrese un apellido: ")
+#             apellidos.append(apellido)
+#         case 2:
+#             # c=0
+#             # for n in nombres:
+#             #     print(nombre[c], apellidos[c])
+#             #     c+=1
+
+#             for i in range(len(nombres)):
+#                 print(nombres[i], apellidos[i])
+#         case 3:
+#             nom=input("ingrese un nombre")
+#             if nom in nombres:
+#                 print(f'El nombre {nom} existe')
+#             else:
+#                 print(f'El nombre {nom} no existe')
+#         case 4:
+#             print("saliendo")
+#             break
+#         case _:
+#             print("opción invalida")
+
+#                '''carrito 3.0'''
